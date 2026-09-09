ce513a3564ac45bb0d9f6fd4a783bc4aefdc6c8d
aviral-dot <aviralbharti832002@gmail.com>
2026-09-09 22:33:48 +0530

    persistance neon

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -1,36 +1,7 @@
-
-# # mypy: ignore-errors
-
-# import os
-
-# from langgraph.checkpoint.postgres import PostgresSaver
-
-
-# def create_checkpointer() -> PostgresSaver:
-#     database_url = os.getenv("DATABASE_URL")
-
-#     if not database_url:
-#         raise RuntimeError(
-#             "DATABASE_URL environment variable is not configured."
-#         )
-
-#     checkpointer = PostgresSaver.from_conn_string(
-#         database_url
-#     )
-
-#     checkpointer.setup()
-
-#     return checkpointer
-
-
-# mypy: ignore-errors
-
 import os
+from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
-from typing import AsyncIterator
-
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
-
 
 @asynccontextmanager
 async def create_checkpointer() -> AsyncIterator[AsyncPostgresSaver]:
